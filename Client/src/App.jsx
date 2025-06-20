420e5245f67fcb8c6b6c19c3aee8ffa0a2cb0f34
Krishna Chamarthy <chamarthysr@gmail.com>
2025-06-21 00:07:07 +0530

    21/06/25

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Vite + React!</h1>
+    </div>
+  )
+}
+
+export default App
